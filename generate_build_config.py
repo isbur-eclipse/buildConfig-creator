5689af54df1c9aac81170ca187cd7f537396e280
Igor Burenkov <isburenkov@gmail.com>
2020-01-31 06:44:04 +0000

    Successfully created buildConfig from jinja2 template

---
+++ generate_build_config.py
@@ -0,0 +1,26 @@
+import os
+
+#Import necessary functions from Jinja2 module
+from jinja2 import Environment, FileSystemLoader
+
+#Import YAML module
+import yaml
+
+#Load data from YAML into Python dictionary
+config_data = yaml.load(open('./settings.yaml'))
+
+
+#Load Jinja2 template
+env = Environment(loader = FileSystemLoader('.'), trim_blocks=True, lstrip_blocks=True)
+
+def path_join(path_components):
+  return os.path.join(*path_components)
+env.filters['path_join']=path_join
+
+template = env.get_template('ancestor-build-config.yaml')
+
+
+
+
+#Render the template with data and print the output
+print(template.render(config_data))
---
+++ main.py
@@ -0,0 +1,1 @@
+import generate_build_config
